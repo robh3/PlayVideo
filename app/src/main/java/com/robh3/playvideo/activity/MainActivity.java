4c6beb7b21fb255c0f298d106ff1b4cfb725ceb2
Rob Howard <mr.robhoward@gmail.com>
2016-01-13 23:43:40 -0600

    Commit

--- app/src/main/java/com/robh3/playvideo/activity/MainActivity.java
+++ app/src/main/java/com/robh3/playvideo/activity/MainActivity.java
@@ -1,15 +1,35 @@
 package com.robh3.playvideo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.robh3.playvideo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
         setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.MyButton);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        VideoViewActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
