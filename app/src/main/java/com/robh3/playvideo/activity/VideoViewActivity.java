714681e8d76feb2d558dfb9f8851346581342680
Rob Howard <mr.robhoward@gmail.com>
2016-01-14 00:18:37 -0600

    Commit

--- app/src/main/java/com/robh3/playvideo/activity/VideoViewActivity.java
+++ app/src/main/java/com/robh3/playvideo/activity/VideoViewActivity.java
@@ -24,50 +24,27 @@ public class VideoViewActivity extends AppCompatActivity {
     VideoView videoview;
 
     // Insert your Video URL
-    String VideoURL = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+    String VideoURL = "https://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the layout from video_main.xml
         setContentView(R.layout.videoview_main);
-        // Find your VideoView in your video_main.xml layout
-        videoview = (VideoView) findViewById(R.id.VideoView);
-        // Execute StreamVideo AsyncTask
 
-        // Create a progressbar
-        pDialog = new ProgressDialog(VideoViewActivity.this);
-        // Set progressbar title
-        pDialog.setTitle("Android Video Streaming Tutorial");
-        // Set progressbar message
-        pDialog.setMessage("Buffering...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(false);
-        // Show progressbar
-        pDialog.show();
-
-        try {
-            // Start the MediaController
-            MediaController mediacontroller = new MediaController(VideoViewActivity.this);
-            mediacontroller.setAnchorView(videoview);
-            // Get the URL from String VideoURL
-            Uri video = Uri.parse(VideoURL);
-            videoview.setMediaController(mediacontroller);
-            videoview.setVideoURI(video);
-
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-        }
-
-        videoview.requestFocus();
-        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        final VideoView mVideoView = (VideoView) findViewById(R.id.VideoView);
+        mVideoView.setVideoPath(VideoURL);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(mVideoView);
+        //mVideoView.setMediaController(mediaController);
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new OnPreparedListener() {
             // Close the progress bar and play the video
             public void onPrepared(MediaPlayer mp) {
-                pDialog.dismiss();
-                videoview.start();
+                mVideoView.start();
             }
         });
+
 
     }
 }
